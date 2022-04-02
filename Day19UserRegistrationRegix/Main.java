0c4b422986e16063ba336a295f0795673f8680e6
PoojaSolankar <poojasolankar09gmail.com>
2022-04-03 01:15:48 +0530

    Validate First Name

---
+++ Day19UserRegistrationRegix/Main.java
@@ -0,0 +1,15 @@
+package com.Bridgelabz.Day19UserRegistrationRegix;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		UserRegistration userregistration = new UserRegistration();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the FirstName");
+		String firstName = sc.nextLine();
+		userregistration.validateFirstName(firstName);
+		
+	}	
+}
